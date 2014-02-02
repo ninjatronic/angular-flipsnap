8cd4083d392e68cc5a14cab73a498bffd473f502
Pete Martin <pete@ninjatronic.com>
2014-02-02 09:24:42 +0100

    Updates the flipsnapper when a contained ngRepeat is updated

--- angular-flipsnap.js
+++ angular-flipsnap.js
@@ -19,6 +19,9 @@
                     var hiddenReset = $flipsnap[0].hidden;
                     $flipsnap[0].hidden = true;
 
+                    var repeatWatchers = {};
+                    var optionsWatch;
+
                     function hasAttribute(element, attrName) {
                         var attr = element.attr(attrName);
                         return attr != undefined || attr != null;
@@ -28,29 +31,51 @@
                         return hasAttribute(element, 'flipsnap-pane');
                     }
 
-                    function completeLayout() {
+                    function isNgRepeat(element) {
+                        return hasAttribute(element, 'ng-repeat');
+                    }
+
+                    $scope.completeLayout = function() {
                         $flipsnap[0].hidden = hiddenReset;
 
                         var totalWidth = 0;
                         angular.forEach($flipsnap.children(), function(child) {
                             child = angular.element(child);
+
                             if(isFlipsnapPane(child)) {
                                 totalWidth += child[0].offsetWidth;
                                 child.css('float', 'left');
                             }
+
+                            if(isNgRepeat(child)) {
+                                console.log('got repeater');
+                                var target = child.attr('ng-repeat').match(/in\s+(.+)/)[1];
+                                if(!repeatWatchers[target]) {
+                                    repeatWatchers[target] = $scope.$parent.$watch(target, function() {
+                                        $timeout(function() {
+                                            $scope.completeLayout();
+                                            $scope.generateFlipsnap();
+                                        });
+                                    }, true);
+                                }
+                            }
                         });
+
+                        console.log('going to set the width to: '+totalWidth);
 
                         $element.css('overflow', 'hidden');
                         $flipsnap.css('width', (totalWidth)+'px');
                         $flipsnap.attr('id', id);
 
-                        $scope.$parent.$watch($attrs.flipsnapOptions, function(newValue) {
-                            console.log(newValue);
-                            $scope.$parent.flipsnap = $window.Flipsnap('#'+id, options);
-                        }, true);
-                    }
+                        optionsWatch = optionsWatch || $scope.$parent.$watch($attrs.flipsnapOptions, $scope.generateFlipsnap, true);
+                    };
 
-                    $timeout(completeLayout);
+                    $scope.generateFlipsnap = function() {
+                        console.log('generating Flipsnap');
+                        $scope.$parent.flipsnap = $window.Flipsnap('#'+id, options);
+                    };
+
+                    $timeout($scope.completeLayout);
                 }
             };
 
